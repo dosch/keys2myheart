c62e5077cfe255c932551cf3881ae1e76ab624e2
L. Fernandez <luis@greenhost.nl>
2015-04-30 17:18:26 +0200

    now using the python gnupg wrapper

--- gpg-batch/batchgpg.py
+++ gpg-batch/batchgpg.py
@@ -5,6 +5,8 @@ import json
 import re
 import pipes
 from subprocess import Popen, PIPE
+import shutil, time
+import gnupg
 
 KEYSPEC = """
 %echo Generating an OpenPGP key
@@ -16,12 +18,11 @@ Name-Real: {name}
 Name-Comment: {comment}
 Name-Email: {email}
 Expire-Date: {expire}
-Passphrase: {passphrase}
-%pubring {pubring}
-%secring {secring}
 %commit
 %echo Key generated. Done.
 """
+
+gpg = gnupg.GPG(homedir="./temp.gnupg")
 
 def default_spec():
 	return KEYSPEC.format(type='RSA', 
@@ -32,7 +33,6 @@ def default_spec():
 							comment='', 
 							email='kermit@invalid.tld', 
 							expire='0', 
-							passphrase='badpassphrase', 
 							pubring='foo.pub', 
 							secring='foo.sec')
 
@@ -41,13 +41,10 @@ def generate_spec(params):
 							length=params["length"], 
 							subtype=params["subtype"], 
 							sublength=params["sublength"], 
-							name=params["name"], 
+							name=params["name_real"], 
 							comment=params["comment"], 
-							email=params["email"], 
-							expire=params["expire"], 
-							passphrase=params["passphrase"], 
-							pubring=params["pubring"], 
-							secring=params["secring"])
+							email=params["name_email"], 
+							expire=params["expire"])
 
 def read_stdin():
 	""" Read configuration from standard input, expects JSON string """
@@ -68,8 +65,14 @@ def check_input(cfgarr, param):
 
 
 def validate_username(uname):
+	""" make sure username is a valid string """
 	return re.match("^[a-zA-Z0-9]+$", uname)
 
+
+def shred_keyring(pubkring, seckring):
+	""" shred the keyring files """
+	for f in (pubkring, seckring):
+		os.system("shred -n 10 -u -z -v {0}".format(f))
 
 def generate_keys(cfgarr):
 	""" Generate GPG keys using a dynamically generated parameter file. For this purpose
@@ -77,24 +80,39 @@ def generate_keys(cfgarr):
 	see man page for further details: https://www.gnupg.org/documentation/manpage.html
 	"""
 	spec = generate_spec(cfgarr)
-	p = os.popen('gpg --batch --gen-key', 'w')
-	p.write(spec)
-	#print spec
-	p.close()
+	begin = time.time()
+	retkey = gpg.gen_key(spec)
+	end = time.time()
+	print "generated key with fp: ", retkey.fingerprint, "in", (end-begin), "seconds"
+
+	pubkey = gpg.export_keys(retkey.fingerprint)
+	seckey = gpg.export_keys(retkey.fingerprint, secret=True)
+
+	print "exported pubkey: ", pubkey
+
+	return retkey
+	# p = os.popen("gpg --batch --gen-key ", 'w')
+	# p.write(spec)
+	# #print spec
+	# p.close()
+
+def get_generated_key(params):
+	""" get key id and fingerprint of generated key """
+	os.popen("gpg --primary-keyring {0} --list-keys --no-default-keyring --homedir .".format(params["pubring"]) )
 
 def main():
 	cfg = read_stdin()
 
 	# check and validate input values
-	check_input(cfg, 'username')
-	if not validate_username(cfg['username']):
-		print "Illegal username"
-		sys.exit(1)
+	# check_input(cfg, 'username')
+	# if not validate_username(cfg['username']):
+	# 	print "Illegal username"
+	# 	sys.exit(1)
 
 	# get username from config object
-	username = cfg['username']
+	#username = cfg['username']
 	generate_keys(cfg)
 
 
 if __name__ == "__main__":
-    main()
+	main()
